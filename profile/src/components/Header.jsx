c96bda53b7a4d63cc7030493b086d97385abb54e
Kris <Krismail>
2023-04-03 22:19:31 +0300

    feat: add css module

--- profile/src/components/Header.jsx
+++ profile/src/components/Header.jsx
@@ -1,7 +1,8 @@
+import classes from './Header.module.css'
 
 export const Header = () => {
     return(
-        <header className='header'>
+        <header className={classes.header}>
         <img src='https://png.pngtree.com/png-clipart/20190322/ourlarge/pngtree-japanese-cherry-blossom-fresh-style-and-fan-png-image_862466.jpg' alt='logo'></img>
       </header>
     )
--- profile/src/components/Profile.jsx
+++ profile/src/components/Profile.jsx
@@ -1,9 +1,10 @@
+import classes from './Profile.module.css'
 
 export const Profile = () => {
     return(
-      <div className='content'>
+      <div className={classes.content}>
           <img alt='japan' src='https://travelclub.kz/storage/tours/August2022/PPotdGnApSOgAdMnveS0.jpg'></img>
-        <div>
+        <div className={classes.content__ava}>
           <img src='https://img.freepik.com/premium-vector/masked-samurai-girl-hand-drawn-illustration-vector_73644-9.jpg?w=2000' alt='samuray'></img>
         </div>
         <div>
@@ -12,8 +13,8 @@ export const Profile = () => {
             new post
           </div>
           <div>
-            <div>Post1</div>
-            <div>Post2</div>
+            <div className='item'>Post1</div>
+            <div className='item'>Post2</div>
           </div>
         </div>
       </div>
