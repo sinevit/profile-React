f668b5ff8f6cbf1627c21d559adba837c7bc0dcb
Kris <Krismail>
2023-04-03 17:12:25 +0300

    feat: add base structure

---
+++ profile/src/components/Header.jsx
@@ -0,0 +1,8 @@
+
+export const Header = () => {
+    return(
+        <header className='header'>
+        <img src='https://png.pngtree.com/png-clipart/20190322/ourlarge/pngtree-japanese-cherry-blossom-fresh-style-and-fan-png-image_862466.jpg' alt='logo'></img>
+      </header>
+    )
+}
---
+++ profile/src/components/Profile.jsx
@@ -0,0 +1,21 @@
+
+export const Profile = () => {
+    return(
+      <div className='content'>
+          <img alt='japan' src='https://travelclub.kz/storage/tours/August2022/PPotdGnApSOgAdMnveS0.jpg'></img>
+        <div>
+          <img src='https://img.freepik.com/premium-vector/masked-samurai-girl-hand-drawn-illustration-vector_73644-9.jpg?w=2000' alt='samuray'></img>
+        </div>
+        <div>
+          my posts 
+          <div>
+            new post
+          </div>
+          <div>
+            <div>Post1</div>
+            <div>Post2</div>
+          </div>
+        </div>
+      </div>
+    )
+}
