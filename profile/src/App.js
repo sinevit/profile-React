370f280b7babab036a51d46f7fce62fdf6767247
Kris <Krismail>
2023-04-19 10:31:21 +0300

    feat: add lazy loading

--- profile/src/App.js
+++ profile/src/App.js
@@ -1,11 +1,18 @@
+import React, { Suspense } from 'react';
 import './App.css';
 import HeaderContainer from './components/Header/HeaderContainer';
 import { Navbar } from './components/Navbar/Navbar';
 import { Routes, Route, BrowserRouter } from 'react-router-dom';
-import DialogsContainer  from './components/Dialogs/DialogsContainer';
-import UsersContainer from './components/Users/UsersContainer';
-import ProfileContainer from './components/Profile/ProfileContainer';
+
+// import DialogsContainer  from './components/Dialogs/DialogsContainer';
+// import UsersContainer from './components/Users/UsersContainer';
+// import ProfileContainer from './components/Profile/ProfileContainer';
 import Login from './components/Login/Login';
+import { Preloader } from './components/common/Preloader/Preloader';
+
+const DialogsContainer = React.lazy(() => import('./components/Dialogs/DialogsContainer'));
+const ProfileContainer = React.lazy(() => import('./components/Profile/ProfileContainer'));
+const UsersContainer = React.lazy(() => import('./components/Users/UsersContainer'));
 
 
 const App = (props) => {
@@ -15,12 +22,15 @@ const App = (props) => {
         <HeaderContainer />
         <Navbar />
         <div className='app-wrapper-content'>
-          <Routes>
-            <Route path='/profile/:userId?' element={<ProfileContainer />} />
-            <Route path='/dialogs' element={<DialogsContainer />} />
-            <Route path='/users' element={<UsersContainer />} />
-            <Route path='/login' element={<Login />} />
-          </Routes>
+          <Suspense fallback={<Preloader />}>
+            <Routes>
+              <Route path='/profile/:userId?' element={<ProfileContainer />} />
+              <Route path='/dialogs' element={<DialogsContainer />} />
+              <Route path='/users' element={<UsersContainer />} />
+              <Route path='/login' element={<Login />} />
+            </Routes>
+          </Suspense>
+
         </div>
       </div>
     </BrowserRouter>
